2251fb93f1c75be2930270ed1a9b785b9312d66c
Alex Krantz <alex@krantz.dev>
2023-06-11 19:16:02 -0700

    feat(database): add users table

--- wafflodo/database.py
+++ wafflodo/database.py
@@ -4,6 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User id={self.id} username={self.username!r}>"
+
+
 class Todo(db.Model):
     __tablename__ = "todos"
 
